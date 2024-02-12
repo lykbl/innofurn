ccd836aab87be162b8c4e39967003e7a1239d78c
lykbl <deslike33@gmail.com>
2024-02-12 23:59:51 +0100

    add dicsounts to rpoducts and variatns

--- app/Domains/Product/Product.php
+++ app/Domains/Product/Product.php
@@ -7,6 +7,7 @@ namespace App\Domains\Product;
 use App\Models\Translatable;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Query\JoinClause;
+use Lunar\Models\Discount;
 use Lunar\Models\Product as BaseProduct;
 
 /** @method Builder withSlug */
@@ -20,5 +21,20 @@ class Product extends BaseProduct implements Translatable
                 ->on('lunar_urls.element_id', '=', 'lunar_products.id')
                 ->where('lunar_urls.element_type', '=', BaseProduct::class)
             );
+    }
+
+    public function discounts()
+    {
+        // TODO add active scope
+        return $this
+            ->hasManyThrough(
+                Discount::class,
+                \Lunar\Models\DiscountPurchasable::class,
+                'purchasable_id',
+                'id',
+                'id',
+                'discount_id'
+            )
+            ->where('lunar_discount_purchasables.purchasable_type', \Lunar\Models\Product::class);
     }
 }
--- app/Domains/ProductVariant/ProductVariant.php
+++ app/Domains/ProductVariant/ProductVariant.php
@@ -7,7 +7,6 @@ namespace App\Domains\ProductVariant;
 use App\Models\Review\Review;
 use App\Models\Translatable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Support\Facades\DB;
 use Lunar\Base\Casts\AsAttributeData;
 use Lunar\Base\Traits\HasAttributes;
 use Lunar\Models\Discount;
@@ -63,69 +62,19 @@ class ProductVariant extends BaseProductVariant implements Translatable
         return $this->id > 2;
     }
 
-    public function getActiveDiscountsAttribute()
+    public function discounts()
     {
-        $productVariantPivots = DB::query()
-            ->select([
-                'lunar_products.id as product_id',
-                'lunar_product_variants.id as product_variant_id',
-                'lunar_collection_product.collection_id as collection_id',
-            ])
-            ->from('lunar_product_variants')
-            ->join('lunar_products', 'lunar_product_variants.product_id', '=', 'lunar_products.id')
-            ->join('lunar_collection_product', 'lunar_products.id', '=', 'lunar_collection_product.product_id')
-            ->where('lunar_product_variants.id', '=', $this->id)
-        ;
-        $collectionHierarchy = DB::query()
-            ->select([
+        // TODO add active scope
+        return $this
+            ->hasManyThrough(
+                Discount::class,
+                \Lunar\Models\DiscountPurchasable::class,
+                'purchasable_id',
                 'id',
-                'parent_id as ord',
-                'product_variant_pivots.collection_id as root_id',
-            ])
-            ->from('lunar_collections')
-            ->join('product_variant_pivots', 'lunar_collections.id', '=', 'product_variant_pivots.collection_id')
-            ->unionAll(
-                DB::query()->select([
-                    'c.id',
-                    'c.parent_id',
-                    'root_id',
-                ])
-                    ->from('lunar_collections as c')
-                    ->join('collection_hierarchy as t', 'c.parent_id', 't.id')
+                'id',
+                'discount_id'
             )
+            ->where('lunar_discount_purchasables.purchasable_type', \Lunar\Models\ProductVariant::class)
         ;
-        $collections = DB::query()
-            ->select([
-                DB::raw('GROUP_CONCAT(id) as collections'),
-                'root_id',
-            ])
-            ->from('collection_hierarchy')
-            ->groupBy('root_id')
-        ;
-        $discountPivots = DB::query()
-            ->select('*')
-            ->withExpression('product_variant_pivots', $productVariantPivots)
-            ->withRecursiveExpression('collection_hierarchy', $collectionHierarchy)
-            ->withRecursiveExpression('collections', $collections)
-            ->from('product_variant_pivots as pvp')
-            ->leftJoin('collections', 'collections.root_id', '=', 'pvp.collection_id')
-        ;
-
-        $discountsQuery = Discount::query()
-            ->select('lunar_discounts.*')
-            ->withExpression('discount_pivots', $discountPivots)
-            ->join('lunar_discount_purchasables', 'lunar_discounts.id', '=', 'lunar_discount_purchasables.discount_id')
-            ->whereRaw('
-                lunar_discount_purchasables.purchasable_id in (select product_variant_id from discount_pivots) or
-                lunar_discount_purchasables.purchasable_id in (select product_id from discount_pivots)
-            ')
-            ->union(
-                Discount::query()
-                    ->select('lunar_discounts.*')
-                    ->join('lunar_collection_discount', 'lunar_discounts.id', '=', 'lunar_collection_discount.discount_id')
-                    ->whereRaw('lunar_collection_discount.collection_id in (select collections from discount_pivots)')
-            );
-
-        return $discountsQuery->get();
     }
 }
