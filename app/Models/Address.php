a5d992c6aecae5fc2dd3a346c84efceaf0a7dae5
lykbl <deslike33@gmail.com>
2024-04-08 20:57:27 +0200

    review fixes

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -4,8 +4,22 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 use Lunar\Models\Address as LunarAddress;
 
 class Address extends LunarAddress
 {
+    public function user(): HasOneThrough
+    {
+        return $this
+            ->hasOneThrough(
+                User::class,
+                CustomerUserPivot::class,
+                'customer_id',
+                'id',
+                'id',
+                'user_id'
+            )
+        ;
+    }
 }
